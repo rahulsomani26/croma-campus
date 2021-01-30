5e63b83a1b02c7f7da25c067f53267a88568b84c
rahulsomani26 <r.somani.26@gmail.com>
2021-01-30 17:24:08 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,47 @@
+# name = "abosaud"
+# print(len(name))
+
+# '''
+# String indexing
+# []  --> index notation / subscript notation
+# '''
+
+# # print(name[0])
+# # print(name[3])  # s
+# # print(name[6])  # d
+# print(name[-1])  # d
+# print(name[-7])  # d
+
+'''
+Slicing
+[start:end]
+[start:end:step]
+
+'''
+
+# print(msg[:])
+# print(msg)
+
+# print(msg[0:8:2])
+# print(msg[0:8:1])
+# msg = "Hello"
+# print(msg[0:-2])
+# print(msg[-1:-6])
+
+'''
+    Some methods on strings 
+    
+'''
+
+# msg = "hello world"
+# print(msg.upper())
+# print(msg.capitalize())
+# print(msg.title())
+
+
+'''
+some more operations on strings 
+
+1. concatenation 
+
+'''
---
+++ first.py
@@ -0,0 +1,18 @@
+# A comment is something which u dont want the pyhon intereperter to execute . This is just for users
+
+'''
+Multiline comment 
+
+'''
+
+
+"""
+    doc string / Multiline comment 
+"""
+
+num = 10
+print(type(num))
+# print(num)
+
+num = 12.3
+# print(type(num))
